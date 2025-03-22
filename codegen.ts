179d7e3d1f8512d8311aa7a5c20518ea6246602b
Alejandro Heredia <logusgraphics@icloud.com>
2025-03-22 12:59:56 +0100

    fix: update codegen config to include all schema files

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: './graphql/schema/root.graphql',
+  schema: './graphql/schema/**/*.graphql',
   generates: {
     './graphql/generated/types.ts': {
       plugins: ['typescript', 'typescript-resolvers'],
